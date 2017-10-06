45d2c7c7f1b04debd3535b0757c4cfff501102f1
Ryan Smith <ryan.smith@1stdibs.com>
2017-10-06 14:07:59 -0400

    revert

--- server.py
+++ server.py
@@ -2,45 +2,24 @@ from flask import Flask
 app = Flask(__name__)
 from flask import Response
 from flask import request
-import json
 
-# from flask import json
-# from flask import jsonify
-@app.route("/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route("/")
 def root():
-	log_request_data(request)
-	# print request.args
-	# print request.headers
+	print(request.__dict__)
 	return Response("You're at the root.<br/>Nothing's here yo...", status=200, mimetype='text/html')
 
-@app.route("/200", methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route("/200")
 def two_hundred():
-	log_request_data(request)
+	print(request.__dict__)
 	return Response("You got a 200!", status=200, mimetype='text/plain')
 
 
-@app.route("/404", methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route("/404")
 def four_o_four():
-	log_request_data(request)
+	print(request.__dict__)
 	return Response("You got a 404!", status=404, mimetype='text/plain')
 
-@app.route("/500", methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route("/500")
 def five_hundred():
-	log_request_data(request)
+	print(request.__dict__)
 	return Response("You got a 500!", status=500, mimetype='text/plain')
-
-def log_request_data(request):
-	r = {
-		'endpoint' :request.endpoint,
-		'json' : request.get_json(force=True, silent=True),
-		'method' : request.method,
-		'path' : request.path,
-		'full_path' : request.full_path,
-		'script_root' : request.script_root,
-		'url' : request.url,
-		'base_url' : request.base_url,
-		'url_root' : request.url_root,
-		'data' : request.data,
-		'args' : request.args
-	}
-	print(json.dumps(r, default=lambda o: o.__dict__, indent=4, sort_keys=True))
