27a038ecc0221b53fafa845931bb45aceff083bc
Ryan Smith <ryan.smith@1stdibs.com>
2017-10-06 12:28:46 -0400

    parenthese

--- server.py
+++ server.py
@@ -5,10 +5,16 @@ from flask import request
 
 # from flask import json
 # from flask import jsonify
+@app.route("/")
+def two_hundred():
+	print(request.__dict__)
+	# print request.args
+	# print request.headers
+	return Response("You're at the root.<br/>Nothing's here yo...", status=200, mimetype='text/html')
 
 @app.route("/200")
 def two_hundred():
-	print request.__dict__
+	print(request.__dict__)
 	# print request.args
 	# print request.headers
 	return Response("You got a 200!", status=200, mimetype='text/plain')
@@ -16,10 +22,10 @@ def two_hundred():
 
 @app.route("/404")
 def four_o_four():
-	print request.__dict__
+	print(request.__dict__)
 	return Response("You got a 404!", status=404, mimetype='text/plain')
 
 @app.route("/500")
 def five_hundred():
-	print request.__dict__
+	print(request.__dict__)
 	return Response("You got a 500!", status=500, mimetype='text/plain')
